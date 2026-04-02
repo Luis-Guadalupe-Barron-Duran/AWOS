0e438ca5864e7ef8f29ffaa36ecc94ab3099112e
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2026-04-01 19:04:32 -0600

    Refactor database connection to use DATABASE_URLbd

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.on('connnect', () => console.log('Conexion exitosa'));
+pool.on('connect', () => console.log('Conexion exitosa'));
 
 module.exports = pool;
